c45d63f117881694d79dca4b5e01ca6fa195a2ce
itsRaCl <rajclerk.2004@gmail.com>
2026-01-05 10:00:46 +0530

    added neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -2,29 +2,29 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-    's1n7ax/nvim-window-picker',
-  },
-  lazy = false,
-  keys = {
-    { '<leader>n', ':Neotree focus<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-    hijack_netrw_behaviour = 'disabled',
-  },
-  source_selector = {
-    statusline = true,
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		"s1n7ax/nvim-window-picker",
+	},
+	lazy = false,
+	keys = {
+		{ "<leader>n", ":Neotree focus<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+			hijack_netrw_behavior = "open_current",
+		},
+	},
+	source_selector = {
+		statusline = true,
+	},
 }
